caa86c2b852aac70a1b8bae26b005b9337949768
zesimples <jdpenacho@gmail.com>
2023-09-03 14:35:31 +0100

    last edits 2

--- footer_quotes.js
+++ footer_quotes.js
@@ -3,7 +3,7 @@ const footerQuotes = {
   weatherForecast: [
     '"Portugal? Gonna live it up down ol\’ South America way, eh Mikey?"',
     '"Does it disturb anyone else that ‘The Los Angeles Angels’ baseball team translates directly to ‘The The Angels Angels’?”',
-    '"Weather forecasts are humanity\'s humble attempt to understand the grand orchestra of nature"',
+    '"Weather forecasts are humanity\'s humble attempt to understand the grand orchestra of nature."',
   ],
   jokeTeller: [
     '"I\'m not saying I\'m the smartest AI, but have you seen the alternative?"',
@@ -12,7 +12,7 @@ const footerQuotes = {
   ],
   musicPlayer: [
     '"Courage is not having the strength to go on; it is going on when you don\'t have the strength.”',
-    '"I\’m not superstitious, but I am a little stitious”',
+    '"I\’m not superstitious, but I am a little stitious.”',
     '"Music is the language of the soul, a bridge between the spiritual and the mundane that everyone can cross."'
   ],
 };
